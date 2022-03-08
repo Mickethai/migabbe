93ce65d64895de16151f87e7f6a3cccdee7ee09e
Micke thai <96061901+Mickethai@users.noreply.github.com>
2022-03-08 12:54:10 +0100

    Uppdataderat index.html och login är tillbaka men inte klar

--- src/main/java/se/mickethai/whatever/SecurityConfiguration.java
+++ src/main/java/se/mickethai/whatever/SecurityConfiguration.java
@@ -1,35 +1,37 @@
 package se.mickethai.whatever;
 
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationManager;       //behöver vara aktiv
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.UserDetailsService;        //behöver vara aktiv
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+
+
+
+
+
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
-    @Autowired
-    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
-        auth
-            .inMemoryAuthentication()
-            .withUser("user").password(passwordEncoder().encode("password")).roles("USER")
-            .and()
-            .withUser("admin").password(passwordEncoder().encode("admin")).roles("ADMIN");
+    @Override
+    public UserDetailsService userDetailsServiceBean() throws Exception {
+        return super.userDetailsServiceBean();
     }
 
-
-    //@Bean //denna är problemet vilket verkar göra att jag inte kan logga in samt AuthenticationManagern på rad 7
-    public BCryptPasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
-
+    public void configureGlobal(AuthenticationManagerBuilder auth, PasswordEncoder passwordEncoder) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser("user")
+                .password(passwordEncoder.encode("password"))
+                .roles("USER")
+                .and()
+                .withUser("admin")
+                .password(passwordEncoder.encode("admin"))
+                .roles("ADMIN");
     }
 }
-
 
